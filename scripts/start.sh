717c2685aacd7c009d2b231ce5786fa6d5f6e5d4
mucek4 <tomaz@gorenc.org>
2019-04-03 12:49:31 +0200

    Change KVM device owner

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -c /dev/kvm ]; then
+  echo "Device KVM exists. Setting rights...."
+  chown 1000:1000 /dev/kvm
+else
+  echo "/dev/kvm does not exist"
+  exit 1
+fi
+
 echo avdmanager --verbose create avd -f -c 50M -k $ANDROID_IMAGE -n android-${ANDROID_API_VERSION} -g google_apis -b $ANDROID_ARCH -d "Nexus 4"
 sudo -u jenkins /opt/android-sdk-linux/tools/bin/avdmanager --verbose create avd -f -c 50M -k $ANDROID_IMAGE -n android-${ANDROID_API_VERSION} -g google_apis -b $ANDROID_ARCH -d "Nexus 4"
 
